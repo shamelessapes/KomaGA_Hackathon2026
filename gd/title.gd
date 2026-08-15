d9ef5565efad4857a9ecc4e18c1c0c3e1619af98
shamelessapes <razaniamimizu@gmail.com>
2026-08-15 18:03:41 +0900

    ふのが作ったタイトル画面を微修正

--- gd/title.gd
+++ gd/title.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@onready var btn_nyuin = $"Button nyuin"    
-@onready var btn_byousitu = $"Button byousitu"  
-@onready var btn_asobi = $"Button asobi"    
-@onready var btn_taiin = $"Button taiin"      
+@onready var btn_nyuin = $"Control/Button nyuin"    
+@onready var btn_byousitu = $"Control/Button byousitu"  
+@onready var btn_asobi = $"Control/Button asobi"    
+@onready var btn_taiin = $"Control/Button taiin"      
 
 func _ready() -> void:
-	Global.fade_in()
+	#Global.fade_in(Color.BLACK)
 
 	btn_nyuin.pressed.connect(_on_nyuin_pressed)
 	btn_byousitu.pressed.connect(_on_byousitu_pressed)
